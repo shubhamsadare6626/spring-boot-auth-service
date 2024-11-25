8623bbbd1934b91d80580d0eb4dac819c15b4e10
shubhamsadare6626 <shubhamsadaress@gmail.com>
2024-11-25 14:51:44 +0530

    update docker files for image building

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM openjdk:17-jdk-alpine
 RUN apk add --no-cache tzdata
 ENV TZ=Asia/Kolkata
 
-COPY target/auth-service-1.0.0.jar /app.jar
+COPY target/spring-boot-auth-service-1.0.0.jar /app.jar
 COPY src/main/resources/application.yml /application.yml
 ENTRYPOINT exec java -server -Xmx256m -Xss1024k -Djava.security.egd=file:/dev/./urandom -jar /app.jar
 
--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 git pull origin main && \
-mvn spring-boot.run  && \
+mvn spring-boot:run  && \
 docker-compose down && \
-docker build -t personal/spring-auth-service . && \
+docker build -t personal/spring-boot-auth-service . && \
 docker image prune -f && docker-compose up -d && \
-docker logs -f spring-auth-service
-
+docker logs -f spring-boot-auth-service
